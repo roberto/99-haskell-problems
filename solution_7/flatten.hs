aa5cf38927bd7cedd1085dbdd4d3de14db7ea380
Roberto Soares <roberto.tech@gmail.com>
2014-08-24 23:09:13 -0300

    solution 7: flatten

---
+++ solution_7/flatten.hs
@@ -0,0 +1,6 @@
+data NestedList a = Elem a | List [NestedList a]
+
+flatten :: NestedList a -> [a]
+flatten (Elem a) = [a]
+flatten (List []) = []
+flatten (List (x:xs)) = flatten x ++ flatten (List xs)

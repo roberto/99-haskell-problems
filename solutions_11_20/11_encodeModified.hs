c76e28366c6c89a4b6334150127d0041ebe19874
Roberto Soares <roberto.tech@gmail.com>
2014-09-14 03:29:02 -0300

    solution 11: minor refactoring

--- solutions_11_20/11_encodeModified.hs
+++ solutions_11_20/11_encodeModified.hs
@@ -3,9 +3,7 @@ data Element a = Multiple Int a | Single a deriving Show
 encodeModified :: Eq a => [a] -> [Element a]
 encodeModified [] = []
 encodeModified (x:xs) = (encoded : encodeModified(rest)) where
-  process = span (==x) (x:xs)
-  collected = fst process
-  rest = snd process
+  (collected, rest) = span (==x) (x:xs)
   encoded
     | length collected == 1 = Single x
     | otherwise = Multiple (length collected) x
